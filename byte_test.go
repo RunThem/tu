f546e6314b47d6651cacafb6adbb306ac255cc57
RunThem <iccy.fun@outlook.com>
2024-04-13 19:29:08 +0800

    update byte utils function

--- byte_test.go
+++ byte_test.go
@@ -6,129 +6,155 @@ import (
 )
 
 func TestIsAlnum(t *testing.T) {
-	assert.True(t, IsAlnum('a'))
-	assert.True(t, IsAlnum('z'))
-	assert.True(t, IsAlnum('A'))
-	assert.True(t, IsAlnum('Z'))
-	assert.True(t, IsAlnum('0'))
-	assert.True(t, IsAlnum('9'))
+	a := assert.New(t)
 
-	assert.False(t, IsAlnum('!'))
+	a.True(IsAlnum('a'))
+	a.True(IsAlnum('z'))
+	a.True(IsAlnum('A'))
+	a.True(IsAlnum('Z'))
+	a.True(IsAlnum('0'))
+	a.True(IsAlnum('9'))
+
+	a.False(IsAlnum('!'))
 }
 
 func TestIsAlpha(t *testing.T) {
-	assert.True(t, IsAlpha('a'))
-	assert.True(t, IsAlpha('z'))
-	assert.True(t, IsAlpha('A'))
-	assert.True(t, IsAlpha('Z'))
+	a := assert.New(t)
 
-	assert.False(t, IsAlpha('0'))
+	a.True(IsAlpha('a'))
+	a.True(IsAlpha('z'))
+	a.True(IsAlpha('A'))
+	a.True(IsAlpha('Z'))
+
+	a.False(IsAlpha('0'))
 }
 
 func TestIsLower(t *testing.T) {
-	assert.True(t, IsLower('a'))
-	assert.True(t, IsLower('z'))
+	a := assert.New(t)
 
-	assert.False(t, IsLower('0'))
+	a.True(IsLower('a'))
+	a.True(IsLower('z'))
+
+	a.False(IsLower('0'))
 }
 
 func TestIsUpper(t *testing.T) {
-	assert.True(t, IsUpper('A'))
-	assert.True(t, IsUpper('Z'))
+	a := assert.New(t)
 
-	assert.False(t, IsUpper('0'))
+	a.True(IsUpper('A'))
+	a.True(IsUpper('Z'))
+
+	a.False(IsUpper('0'))
 }
 
 func TestIsDigit(t *testing.T) {
-	assert.True(t, IsDigit('0'))
-	assert.True(t, IsDigit('9'))
+	a := assert.New(t)
 
-	assert.False(t, IsDigit('a'))
+	a.True(IsDigit('0'))
+	a.True(IsDigit('9'))
+
+	a.False(IsDigit('a'))
 }
 
 func TestIsXdigit(t *testing.T) {
-	assert.True(t, IsXdigit('0'))
-	assert.True(t, IsXdigit('9'))
+	a := assert.New(t)
 
-	assert.True(t, IsXdigit('a'))
-	assert.True(t, IsXdigit('f'))
-	assert.True(t, IsXdigit('A'))
-	assert.True(t, IsXdigit('F'))
+	a.True(IsXdigit('0'))
+	a.True(IsXdigit('9'))
 
-	assert.False(t, IsXdigit('g'))
+	a.True(IsXdigit('a'))
+	a.True(IsXdigit('f'))
+	a.True(IsXdigit('A'))
+	a.True(IsXdigit('F'))
+
+	a.False(IsXdigit('g'))
 }
 
 func TestIsCntrl(t *testing.T) {
-	assert.True(t, IsCntrl(0x1f))
-	assert.True(t, IsCntrl(0x7f))
+	a := assert.New(t)
 
-	assert.False(t, IsCntrl('!'))
+	a.True(IsCntrl(0x1f))
+	a.True(IsCntrl(0x7f))
+
+	a.False(IsCntrl('!'))
 }
 
 func TestIsGraph(t *testing.T) {
-	assert.True(t, IsGraph('!'))
-	assert.True(t, IsGraph('~'))
+	a := assert.New(t)
 
-	assert.False(t, IsGraph('\t'))
+	a.True(IsGraph('!'))
+	a.True(IsGraph('~'))
+
+	a.False(IsGraph('\t'))
 }
 
 func TestIsSpace(t *testing.T) {
-	assert.True(t, IsSpace('\t'))
-	assert.True(t, IsSpace('\r'))
+	a := assert.New(t)
 
-	assert.True(t, IsSpace(' '))
+	a.True(IsSpace('\t'))
+	a.True(IsSpace('\r'))
 
-	assert.False(t, IsSpace('1'))
+	a.True(IsSpace(' '))
+
+	a.False(IsSpace('1'))
 }
 
 func TestIsBlank(t *testing.T) {
-	assert.True(t, IsBlank('\t'))
-	assert.True(t, IsBlank(' '))
+	a := assert.New(t)
 
-	assert.False(t, IsBlank('1'))
+	a.True(IsBlank('\t'))
+	a.True(IsBlank(' '))
+
+	a.False(IsBlank('1'))
 }
 
 func TestIsPrint(t *testing.T) {
-	assert.True(t, IsPrint(' '))
-	assert.True(t, IsPrint('~'))
+	a := assert.New(t)
 
-	assert.False(t, IsPrint('\t'))
+	a.True(IsPrint(' '))
+	a.True(IsPrint('~'))
+
+	a.False(IsPrint('\t'))
 }
 
 func TestIsPunct(t *testing.T) {
-	assert.True(t, IsPunct('!'))
-	assert.True(t, IsPunct('/'))
+	a := assert.New(t)
 
-	assert.True(t, IsPunct(':'))
-	assert.True(t, IsPunct('@'))
+	a.True(IsPunct('!'))
+	a.True(IsPunct('/'))
 
-	assert.True(t, IsPunct('['))
-	assert.True(t, IsPunct('`'))
+	a.True(IsPunct(':'))
+	a.True(IsPunct('@'))
 
-	assert.True(t, IsPunct('{'))
-	assert.True(t, IsPunct('~'))
+	a.True(IsPunct('['))
+	a.True(IsPunct('`'))
 
-	assert.False(t, IsPunct('1'))
+	a.True(IsPunct('{'))
+	a.True(IsPunct('~'))
+
+	a.False(IsPunct('1'))
 }
 
 func TestToLower(t *testing.T) {
+	a := assert.New(t)
 
-	assert.Equal(t, byte('a'), ToLower('A'))
-	assert.Equal(t, byte('z'), ToLower('Z'))
+	a.Equal(byte('a'), ToLower('A'))
+	a.Equal(byte('z'), ToLower('Z'))
 
-	assert.Equal(t, byte('a'), ToLower('a'))
-	assert.Equal(t, byte('z'), ToLower('z'))
+	a.Equal(byte('a'), ToLower('a'))
+	a.Equal(byte('z'), ToLower('z'))
 
-	assert.Equal(t, byte('1'), ToLower('1'))
+	a.Equal(byte('1'), ToLower('1'))
 }
 
 func TestToUpper(t *testing.T) {
+	a := assert.New(t)
 
-	assert.Equal(t, byte('A'), ToUpper('a'))
-	assert.Equal(t, byte('Z'), ToUpper('z'))
+	a.Equal(byte('A'), ToUpper('a'))
+	a.Equal(byte('Z'), ToUpper('z'))
 
-	assert.Equal(t, byte('A'), ToUpper('A'))
-	assert.Equal(t, byte('Z'), ToUpper('Z'))
+	a.Equal(byte('A'), ToUpper('A'))
+	a.Equal(byte('Z'), ToUpper('Z'))
 
-	assert.Equal(t, byte('1'), ToUpper('1'))
+	a.Equal(byte('1'), ToUpper('1'))
 }
